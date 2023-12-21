f7e401fa99fee86089604dd63435620f8e6a486f
anujaadhish <anujaadhish@gmail.com>
2023-12-21 11:52:32 +0530

    update strength

--- task2/src/components/ImgThree.js
+++ task2/src/components/ImgThree.js
@@ -2,7 +2,7 @@ import { Parallax } from 'react-parallax';
 import SpaceStation from '../img/spacestation.jpg'
 
 const ImageOne = () => (
-    <Parallax className='image' bgImage={SpaceStation} strength={200}>
+    <Parallax className='image' bgImage={SpaceStation} strength={400}>
        <div className='content'>
         <span className='img-txt'>a trip to space</span>
        </div>
--- task2/src/components/ImgTwo.js
+++ task2/src/components/ImgTwo.js
@@ -2,7 +2,7 @@ import { Parallax } from 'react-parallax';
 import Satellite from '../img/satellite.jpeg'
 
 const ImageOne = () => (
-    <Parallax className='image' bgImage={Satellite} strength={200}>
+    <Parallax className='image' bgImage={Satellite} strength={600}>
        <div className='content'>
         <span className='img-txt'>a trip to space</span>
        </div>
